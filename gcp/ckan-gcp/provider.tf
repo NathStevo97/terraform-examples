b8e73af608dd421b02ad5cdad08abe3d1de4924c
NathStevo97 <natstephenson15@gmail.com>
2026-04-03 19:49:11 +0100

    chore: tidy

--- gcp/ckan-gcp/provider.tf
+++ gcp/ckan-gcp/provider.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.85.0"
+      version = "7.23.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("${var.key_name}")
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
